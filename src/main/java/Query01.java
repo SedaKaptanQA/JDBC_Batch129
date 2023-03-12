a45cd07b52858943ed988fcedd1a045d3be4584e
Seda Kaptan <sedakaptan3@gmail.com>
2023-03-12 20:05:28 +0300

    Initial commit

---
+++ src/main/java/Query01.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class Query01 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        //1) Driver Tanımla
+        Class.forName("org.postgresql.Driver");
+
+        //2) Database'e Bağlan
+        Connection con = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/jdbc",
+                "postgres",
+                "1234");
+
+        //3) Statement
+        Statement st = con.createStatement();
+        //4)ResultSet
+
+
+        ResultSet veri = st.executeQuery("select * from ogrenciler");
+        //5)Dataları al
+        while(veri.next()){
+            System.out.println(veri.getInt(1)
+            + veri.getString(2)
+            + veri.getString(3)
+            + veri.getString(4));
+        }
+        con.close();
+        st.close();
+        veri.close();
+    }
+}
